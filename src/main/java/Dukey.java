68d569dd5b2adb21089b0bf508d662a4addb7047
Strikerprv <chendongjun555@gmail.com>
2025-08-22 13:35:27 +0800

    Added Echo

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -1,20 +1,33 @@
+import java.util.Scanner;
+
 public class Dukey {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
         String welcome =
                 "____________________________________________________________\n" +
                 " Hello! I'm Dukey\n" +
                 " What can I do for you?\n" +
+                "____________________________________________________________\n";
+        String bye =
                 "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n";
 
-        System.out.println("Hello from\n" + welcome);
+        System.out.println(welcome);
 
+        Scanner sc = new Scanner(System.in);
+        boolean active = true;
+
+        while (active) {
+            String command = sc.nextLine();
+            if (command.toLowerCase().equals("bye")) {
+                System.out.println(bye);
+                active = false;
+            } else {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                                command +
+                                "\n____________________________________________________________\n");
+            }
+        }
     }
 }

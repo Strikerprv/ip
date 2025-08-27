f87c667bf52ce8083740dbbe4119e822b6a2322c
Strikerprv <chendongjun555@gmail.com>
2025-08-27 17:15:28 +0800

    Added mark as done and unmark functionality, also included new class Task

--- src/main/java/Dukey.java
+++ src/main/java/Dukey.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Dukey {
+
     public static void main(String[] args) {
         String welcome =
                 "____________________________________________________________\n" +
@@ -16,7 +17,7 @@ public class Dukey {
         System.out.println(welcome);
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> todoList = new ArrayList<>();
+        ArrayList<Task> todoList = new ArrayList<>();
         boolean active = true;
 
         while (active) {
@@ -25,19 +26,68 @@ public class Dukey {
                 System.out.println(bye);
                 active = false;
             } else if (command.toLowerCase().equals("list")) {
-                System.out.println("____________________________________________________________");
-                for (int count = 1; count <= todoList.size(); count++) {
-                    System.out.println(count + ". " + todoList.get(count-1));
+                Dukey.printList(todoList);
+            } else if (command.split(" ").length == 2 &&
+                    command.split(" ")[0].toLowerCase().equals("mark")) {
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                if (taskNumber <= todoList.size()) {
+                    markDone(todoList, taskNumber);
+                } else {
+                    System.out.println(
+                            "____________________________________________________________\n" +
+                            "Task number exceeds the number of tasks! Please amend command!\n" +
+                            "____________________________________________________________\n");
                 }
-                System.out.println("____________________________________________________________\n");
+            } else if (command.split(" ").length == 2 &&
+                    command.split(" ")[0].toLowerCase().equals("unmark")) {
+                int taskNumber = Integer.parseInt(command.split(" ")[1]);
+                if (taskNumber <= todoList.size()) {
+                    unmarkDone(todoList, taskNumber);
+                } else {
+                    System.out.println(
+                            "____________________________________________________________\n" +
+                                    "Task number exceeds the number of tasks! Please amend command!\n" +
+                            "____________________________________________________________\n");
+                }
             } else {
-                    todoList.add(command);
+                    todoList.add(new Task(command));
                     System.out.println(
                             "____________________________________________________________\n" +
                                     "added: " + command +
                                     "\n____________________________________________________________\n");
             }
         }
+    }
+
+    public static void printList(ArrayList<Task> todoList) {
+        System.out.println("____________________________________________________________");
+        for (int count = 1; count <= todoList.size(); count++) {
+            Task currentTask = todoList.get(count - 1);
+            System.out.println(count + ". [" + currentTask.getStatusIcon() + "] " + currentTask.getDescription());
+        }
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void markDone(ArrayList<Task> todoList, int taskNumber) {
+        Task currentTask = todoList.get(taskNumber - 1);
+        currentTask.markDoneStatus();
+        String successMessage =
+                "____________________________________________________________\n" +
+                        "Nice! I've marked this task as done.\n" +
+                        "[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription() +
+                        "\n____________________________________________________________\n";
+        System.out.println(successMessage);
+    }
+
+    public static void unmarkDone(ArrayList<Task> todoList, int taskNumber) {
+        Task currentTask = todoList.get(taskNumber - 1);
+        currentTask.unmarkDoneStatus();
+        String successMessage =
+                "____________________________________________________________\n" +
+                        "Nice! I've unmarked this task as not done.\n" +
+                        "[" + currentTask.getStatusIcon() + "] " + currentTask.getDescription() +
+                        "\n____________________________________________________________\n";
+        System.out.println(successMessage);
     }
 }
 

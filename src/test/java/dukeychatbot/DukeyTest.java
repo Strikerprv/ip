d2d6c1406d63e74f35b3af28e8776c872b54a971
Strikerprv <chendongjun555@gmail.com>
2025-09-22 03:22:19 +0800

    Fix CI workflow 3

--- src/test/java/dukeychatbot/DukeyTest.java
+++ src/test/java/dukeychatbot/DukeyTest.java
@@ -1,14 +1,14 @@
 package dukeychatbot;
 
+import dukeychatbot.tasktypes.Deadline;
+import dukeychatbot.tasktypes.Event;
+
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-
-import dukeychatbot.tasktypes.Deadline;
-import dukeychatbot.tasktypes.Event;
 
 /**
  * Tests the functionality of components of Dukey chatbot.
